9948e1a16751bb54e8a6047a39dbff5f8ead79d6
SOKO GIFT <32112696+SOXCODE@users.noreply.github.com>
2019-06-10 13:57:15 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -2,4 +2,4 @@ var birthYear = 1986;
 var futureYear  = 2025;
 var age  = futureYear - birthYear;
 
-console.log('I will be ' + age + ' Years in ' + futuerYear);
+console.log('I will be either ' + age + ' or ' + (age - 1));
