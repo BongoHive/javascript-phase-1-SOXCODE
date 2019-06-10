8b731eea0ab943af1af9200ace8139eb6b085acf
SOKO GIFT <32112696+SOXCODE@users.noreply.github.com>
2019-06-10 14:05:38 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -3,3 +3,11 @@ var futureYear  = 2025;
 var age  = futureYear - birthYear;
 
 console.log('I will be either ' + age + ' or ' + (age - 1));
+
+
+var currentAge = 28;
+var maxAge = 100;
+var numPerDay = 70;
+var totalNeeded = (numPerDay * 365) * (maxAge - currentAge);
+var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+console.log(message);
